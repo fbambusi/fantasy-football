e6402f1042650a3b5a573145f935bef6476b45d1
fbambusi <fbambusi@github.com>
2019-08-18 15:47:53 +0200

    Auction simulation

--- auction/bidder.py
+++ auction/bidder.py
@@ -1,20 +1,30 @@
 import random
 
 class bidder:
-	def __init__(self):
+	def __init__(self,budget):
 		self.ownedPlayers=[]
+		self.budget=budget
 	def getBoughtPlayers(self):
 		return []
 	def buyPlayer(self,player,price):
 		self.ownedPlayers.append(player)
 		self.budget-=price
 		return
+
 	def getOwnedPlayers(self):
 		return self.ownedPlayers
+
+	def getNumberOfOwnedPlayers(self):
+		return len(self.ownedPlayers)
+
 	def getBudget(self):
-		return 1
+		return self.budget
+
 	def setBudget(self,budget):
 		self.budget=budget
+
+	def canPay(self,price):
+		return self.budget>=price
 	def getDesiredPlayers(self,playersList):
 		self.availablePlayers=playersList
 		self.desiredPlayers=playersList[:30]
@@ -30,16 +40,25 @@ class bidder:
 		for playerName in self.values.keys():
 			self.values[playerName]+=(winner.getValueOfPlayers()[playerName]-self.values[playerName])*learning_rate
 		
-	def randomlyCorrectValueOfPlayers(self,learning_rate):
+	def randomlyCorrectValueOfPlayers(self,learning_rate,budget):
+		oldPrice=0
 		for playerName in self.values.keys():
-			self.values[playerName]+=random.random()*learning_rate/10
+			self.values[playerName]+=(random.random()-0.5)*learning_rate*3+(oldPrice-self.values[playerName])*learning_rate/4
+
+			if self.values[playerName]>budget:
+				self.values[playerName]=budget
+			if self.values[playerName]<=0:
+				self.values[playerName]=0
+			oldPrice=self.values[playerName]
 
 	def resetOwnedPlayers(self):
 		self.ownedPlayers=[]
 	def randomizeEvaluation(self,playersList):
 		evals={}
+		currEval=random.random()
 		for player in playersList:
-			evals[player.getName()]=random.random()
+			currEval=random.random()
+			evals[player.getName()]=currEval
 		self.values=evals
 
 	def getPerformanceOfTeam(self):
--- auction/simulateAuction.py
+++ auction/simulateAuction.py
@@ -4,41 +4,54 @@ from bidder import bidder
 from player import traditionalPlayer
 
 NBIDDERS=7
-BUDGET=500
+BUDGET=30
 NPLAYERS=100
 
-LEARNING_RATE=0.1
+LEARNING_RATE=0.7
 
+NEEDED_PLAYERS=11
 players=[]
+
+NROUNDS=6000*30
 random.seed(19)
 
 for i in range(NPLAYERS):
-	players.append(traditionalPlayer(i,random.random(),"A"))
+	players.append(traditionalPlayer(i,random.random()**5,"A"))
 players=sorted(players,key=lambda p:p.getPerformance())
 bidders=[]
 
 for i in range(NBIDDERS):
-	bid=bidder()
+	bid=bidder(BUDGET)
 	bid.setBudget(BUDGET)
 	bid.randomizeEvaluation(players)
 	bidders.append(bid)
 
+def everyBidderHasFullTeam(bidders):
+	for bidder in bidders:
+		if bidder.getNumberOfOwnedPlayers()<NEEDED_PLAYERS:
+			return False
+	return True
 
 def getBuyerAndPrice(bidders,desiredPlayer):
 	dest=False
 	price=0
+	
 	for bidder in bidders:
-		if not dest:
-			dest=bidder
-			vls=bidder.getValueOfPlayers()
-			price=vls[desiredPlayer.getName()]
-		else:
-			pls=bidder.getDesiredPlayers(players)
-			if desiredPlayer in pls:
+		if bidder.getNumberOfOwnedPlayers()<=NEEDED_PLAYERS:
+			if not dest:
+				dest=bidder
 				vls=bidder.getValueOfPlayers()
-				if vls[desiredPlayer.getName()]>price:
-					dest=bidder
-					price=price+1
+				price=price+1
+			else:
+				pls=bidder.getDesiredPlayers(players)
+				if desiredPlayer in pls:
+					vls=bidder.getValueOfPlayers()
+					if vls[desiredPlayer.getName()]>price:
+						
+						price2=dest.getValueOfPlayers()[desiredPlayer.getName()]
+						if bidder.canPay(price2):
+							dest=bidder
+							price=price2
 	return dest,price
 	
 
@@ -49,37 +62,70 @@ perfs={}
 for bid in bidders:
 	perfs[bid]=[]
 
-for j in range(800):
+for j in range(NROUNDS):
+	if j%1000==0:
+		print(j)
 	players=pls
 	pls=list(players)
-	for i in range(10):
-		for currentBidder in bidders:
-			desiredPlayer=currentBidder.getDesiredPlayers(players)[0]
-			buyer,price=getBuyerAndPrice(bidders,desiredPlayer)
-			buyer.buyPlayer(desiredPlayer,price)
-			print(buyer,price)
-			players.remove(desiredPlayer)
-			print(desiredPlayer.getName())
 
+	while not everyBidderHasFullTeam(bidders):
+		for currentBidder in bidders:
+			if currentBidder.getNumberOfOwnedPlayers()<NEEDED_PLAYERS:
+				desiredPlayer=currentBidder.getDesiredPlayers(players)[0]
+				buyer,price=getBuyerAndPrice(bidders,desiredPlayer)
+				if buyer:
+					buyer.buyPlayer(desiredPlayer,price)
+							
+					players.remove(desiredPlayer)
+		
 	bidders=sorted(bidders, key=lambda p:p.getPerformanceOfTeam())
-	print("\n\n")	
 	deltas.append(bidders[0].getPerformanceOfTeam()-bidders[-1].getPerformanceOfTeam())
 
 	for bidder in bidders[:-1]:
-		print(bidder.getPerformanceOfTeam())
-
+	
 		bidder.correctValueOfPlayers(bidders[-1],LEARNING_RATE)
 
-	bidders[-1].randomlyCorrectValueOfPlayers(LEARNING_RATE)
+	bidders[-1].randomlyCorrectValueOfPlayers(LEARNING_RATE,BUDGET)
 
 	for bid in bidders:
 		perfs[bid].append(bid.getPerformanceOfTeam())
 		bid.resetOwnedPlayers()
+		bid.setBudget(BUDGET)
 
 import matplotlib.pyplot as plt
 
+'''
 for pf in perfs.keys():
 	pass
 	#plt.plot(perfs[pf])
 plt.plot(deltas)
+plt.show()
+
+for bid in bidders:
+	plt.plot(perfs[bid])
+	break
+
+	
+plt.show()
+'''
+for bid in bidders:
+	vls=bid.getValueOfPlayers()
+	px=[]
+	py=[]
+	for player in pls:
+		px.append(player.getPerformance())
+		py.append(vls[player.getName()])
+#	plt.scatter(px,py)
+
+px=[]
+py=[]
+
+for player in pls:
+	px.append(player.getPerformance())
+	prs=[]
+	for bidder in bidders:
+		prs.append(bidder.getValueOfPlayers()[player.getName()])
+	prs=sorted(prs)
+	py.append(prs[-2])
+plt.scatter(px,py)
 plt.show()
